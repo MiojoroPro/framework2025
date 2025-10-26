6a55a3bbf1f0bf7fc7f5112247df70abbd170b09
Miojoro RAHAJANIAINA <miojoropro@hotmail.com>
2025-10-26 19:10:00 +0300

    sprint2-Bis

---
+++ src/main/java/etu/test/controller/Test2.java
@@ -0,0 +1,12 @@
+package etu.test.controller;
+
+import etu.sprint.framework.annotation.MyUrl;
+import etu.sprint.framework.controller.Controller;
+
+@Controller
+public class Test2 {
+
+    public void hello() {
+        System.out.println("Test2 instance created");
+    }    
+}
