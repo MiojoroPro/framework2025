1bf3735b293c64ead65e99df49ad3a5db07e4b20
Miojoro RAHAJANIAINA <miojoropro@hotmail.com>
2025-11-21 18:56:02 +0300

    sprin-6

--- src/main/java/etu/sprint/framework/RouteMapping.java
+++ src/main/java/etu/sprint/framework/RouteMapping.java
@@ -1,17 +1,43 @@
 package etu.sprint.framework;
 
 import java.lang.reflect.Method;
+import java.util.*;
+import java.util.regex.*;
 
 public class RouteMapping {
 
-    private String pattern;     // ex : /zavatra/{v}
+    private String pattern;
     private Method method;
     private Object controller;
+
+    private Pattern regexPattern;
+    private List<String> variableNames = new ArrayList<>();
 
     public RouteMapping(String pattern, Method method, Object controller) {
         this.pattern = pattern;
         this.method = method;
         this.controller = controller;
+
+        compileRoute(pattern);
+    }
+
+    private void compileRoute(String route) {
+
+        Matcher m = Pattern.compile("\\{([^/]+)}").matcher(route);
+        while (m.find()) {
+            variableNames.add(m.group(1));
+        }
+
+        String regex = route.replaceAll("\\{[^/]+}", "([^/]+)");
+        this.regexPattern = Pattern.compile("^" + regex + "$");
+    }
+
+    public Pattern getRegexPattern() {
+        return regexPattern;
+    }
+
+    public List<String> getVariableNames() {
+        return variableNames;
     }
 
     public String getPattern() {
